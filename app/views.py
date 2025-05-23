89731fb373d0d31a6de73a8945fb8117615d1733
DESKTOP-L02DDSI\notebiz048 <user>
2025-05-23 17:30:40 +0900

    todo 완료기능 구현

--- app/views.py
+++ app/views.py
@@ -33,3 +33,13 @@ def todo_edit(request, pk):
     else:
         form = TodoForm(instance=todo)
     return render(request, 'todo_post.html', {'form': form})
+
+def done_list(request):
+    dones = Todo.objects.filter(complete=True)
+    return render(request, 'done_list.html', {'dones': dones})
+
+def todo_done(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.complete = True
+    todo.save()
+    return redirect('todo_list')
