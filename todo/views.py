c9dca40a04ff0ef91d181b3ad49511d89b97cfcd
DESKTOP-L02DDSI\notebiz048 <user>
2025-05-26 16:04:57 +0900

    Todo 상세 조회 API

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,20 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 
 from .models import Todo
-from .serializer import TodoSimpleSerializer
+from .serializer import TodoSimpleSerializer, TodoDetailSerializer
 
 # Create your views here.
 class TodosAPIView(APIView):
     def get(self, request):
         todos = Todo.objects.filter(complete=False)
         serializer = TodoSimpleSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class TodoAPIView(APIView):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, id=pk)
+        serializer = TodoDetailSerializer(todo)
         return Response(serializer.data, status=status.HTTP_200_OK)
