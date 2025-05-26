a2c2bfbe478773054533fdc6dcd2db48c590cdea
DESKTOP-L02DDSI\notebiz048 <user>
2025-05-26 15:49:10 +0900

    Todo 전체 조회 API

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Todo
+from .serializer import TodoSimpleSerializer
 
 # Create your views here.
+class TodosAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.filter(complete=False)
+        serializer = TodoSimpleSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
