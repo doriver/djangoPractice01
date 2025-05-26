c502de183fa605498989e12f710bc645bf8a4ae7
DESKTOP-L02DDSI\notebiz048 <user>
2025-05-26 12:42:17 +0900

    함수 기반 뷰로 했었는데, 똑같이 클래스 기반 뷰 만들어봄

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import helloApi, booksAPI, bookAPI
+from .views import helloApi, booksAPI, bookAPI, BookAPI, BooksAPI
 
 urlpatterns = [
     path("hello/", helloApi),
-    path("fbv/books", booksAPI),
-    path("fbv/book/<int:bid>", bookAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>/", bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
 ]
