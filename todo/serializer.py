850be6efa2cc1c5011fa78d1b51e9ef2da3bba8d
DESKTOP-L02DDSI\notebiz048 <user>
2025-05-28 17:49:35 +0900

    데모영상

--- todo/serializer.py
+++ todo/serializer.py
@@ -17,4 +17,4 @@ class TodoDetailSerializer(serializers.ModelSerializer):
 class TodoCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('title', 'description')
+        fields = ('title', 'description', 'important')
